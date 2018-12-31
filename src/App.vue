02eb16bd02fe3f7e13b76d384d6101c096e0b9ca
Juan Valera <juan.valera@gmail.com>
2018-12-30 20:23:33 -0600

    Remove BootstrapVue, Added normal Bootstrap, Added Styled Icons

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,87 @@
 
 <template>
+
   <div id="app">
-    <b-navbar toggleable="md" fixed="top" sticky="true" type="dark">
-        <b-navbar-brand>EY</b-navbar-brand>
-        <b-nav-text>In<strong>TAX</strong>lligence</b-nav-text>
-    
-        <b-collapse is-nav id="nav_collapse">
-    
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
+
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-40">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">EY In<strong><TAX/strong>lligence</a>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#">Sign out</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+      
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+          
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <span data-feather="home"></span>
+                  Inicio <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file"></span>
+                  Cargar
+                </a>
+              </li>
+            </ul>
+
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"><span>Últimos</span></h6>
+            <ul class="nav flex-column mb-2">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  2018-12-30
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  2018-12-29
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
         
-              <b-nav-item-dropdown right>
-                <!-- Using button-content slot -->
-                <template slot="button-content">
-                  <em>Usuario</em>
-                </template>
-                <b-dropdown-item href="#">Perfil</b-dropdown-item>
-                <b-dropdown-item href="#">Cerrar</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
+
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
         
-        </b-collapse>
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+            <h1 class="h2">Dashboard</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+              <div class="btn-group mr-2">
+                <button class="btn btn-sm btn-outline-secondary">Share</button>
+                <button class="btn btn-sm btn-outline-secondary">Export</button>
+              </div>
+              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                <span data-feather="calendar"></span>
+                This week
+              </button>
+            </div>
+          </div>
+
+          <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
+
+          <HelloWorld />
+          <div class="alert alert-danger" role="alert">
+            This is a danger alert—check it out!
+          </div>
+
+        </main>
         
-    </b-navbar>
-    
-    <b-container>
-        <b-row class="h-100" style="background-color=#441122">
-            <b-col cols="3" class="sidebar">
-                <div class="h-100" style="background-color=#44ff66">
-                    Sidebar
-                </div>
-            </b-col>
-            <b-col>
-                <HelloWorld />
-                <b-alert show>Default Alert</b-alert>
-            </b-col>
-        </b-row>
-    </b-container>
-  
+      </div>
+    </div>
+
   </div>
+  
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import Vue from "vue";
-import BootstrapVue from "bootstrap-vue";
 import App from "./App";
-
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
