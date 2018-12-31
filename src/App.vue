b27f420bd64a5c99170f96d583f91d62de36b318
Juan Valera <juan.valera@gmail.com>
2018-12-30 20:32:47 -0600

    FIX: Syntax error

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div id="app">
 
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-40">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">EY In<strong><TAX/strong>lligence</a>
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">EY In<strong>TAX</strong>lligence</a>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
           <a class="nav-link" href="#">Sign out</a>
