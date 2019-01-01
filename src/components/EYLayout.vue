f15c6d32f59a0a44c18c803539f09cb51d70e3f5
Juan Valera <juan.valera@gmail.com>
2019-01-01 11:34:55 -0600

    FIX: Only one component por template

--- src/components/EYLayout.vue
+++ src/components/EYLayout.vue
@@ -1,12 +1,14 @@
 
 <template>
-  <EYNavBar :title="title" />
-  <div class="container-fluid" style="margin-top:200; padding-top:200;" id="main-container">
-    <div class="row">      
-      <EYSideBar/>
-      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-        <slot></slot>
-      </main>
+  <div>
+    <EYNavBar :title="title" />
+    <div class="container-fluid" style="margin-top:200; padding-top:200;" id="main-container">
+      <div class="row">      
+        <EYSideBar/>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+          <slot></slot>
+        </main>
+      </div>
     </div>
   </div>
 </template>
