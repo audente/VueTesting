28822ab378c338c0d6b96b3cb894eaedb182b486
Juan Valera <juan.valera@gmail.com>
2018-12-31 23:47:41 -0600

    Tweak: Routes

--- src/main.js
+++ src/main.js
@@ -23,17 +23,17 @@ const router = new VueRouter({
   ]
 })
 
-/*
+
 new Vue({
   router
 }).$mount('#app')
-*/
 
 
+/*
 new Vue({
   el: "#app",
   components: { App },
   template: "<App/>"
 });
-
+*/
 
