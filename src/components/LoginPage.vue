84959b3207495a33118e5935ab2d1df589cff3a1
Juan Valera <juan.valera@gmail.com>
2019-01-01 13:03:18 -0600

    ADD. Login Form

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -2,7 +2,24 @@
   <EYLayout :title="title">
     <template slot="SideBar"><div></div></template>
     <div id="LoginPage">
-      <h1>{{ msg }}</h1>
+      <h1>{{ Login }}</h1>
+        <form action="">
+          <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" class="form-control" id="email">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+          </div>
+          <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+          </div>
+          <div class="checkbox">
+            <label><input type="checkbox"> Remember me</label>
+          </div>
+          <button type="reset" class="btn btn-primary">Limpiar</button>
+          <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
     </div>
   </EYLayout>
 </template>
@@ -20,7 +37,6 @@ export default {
   },
   data() {
     return {
-      msg: "Hello beauty Vue.js!",
     };
   }
 };
