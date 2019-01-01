f1a243012726a4868f4949ce6e0fb32db1871f47
Juan Valera <juan.valera@gmail.com>
2018-12-31 20:35:07 -0600

    EY Components
    - Added: EYNavBar
    - Added: EYSideBar

--- src/App.vue
+++ src/App.vue
@@ -3,57 +3,12 @@
 
   <div id="app">
 
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap m-0 p-0">
-      <h2 class="navbar-brand col-sm-3 col-md-2">EY</h2>
-      <span class="navbar-text">In<strong>TAX</strong>lligence</span>
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <button type="button" class="btn btn-outline-info">
-            Salir »
-          </button>
-        </li>
-      </ul>
-    </nav>
-
+    <EYNavBar title="Declaración" />
+    
     <div class="container-fluid" style="margin-top:200; padding-top:200;" id="main-container">
       <div class="row">
       
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-          
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  <feather-home class="feather" />
-                  Inicio <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <feather-file class="feather" />
-                  Cargar
-                </a>
-              </li>
-            </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"><span>Últimos</span></h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <feather-file-text class="feather" />
-                  2018-12-30
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <feather-file-text class="feather" />
-                  2018-12-29
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        
+        <EYSideBar id="sidebar" />
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
         
@@ -102,87 +57,8 @@ export default {
   stroke: #cccccc;
 }
 
-
-
-/*
- * Sidebar
- */
-
-.sidebar {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 100; /* Behind the navbar */
-  padding: 0;
-  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
-}
-
-#main-container, .sidebar {
+#main-container, #sidebar {
   padding-top: 100px;
-}
-
-
-.sidebar-sticky {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 48px; /* Height of navbar */
-  height: calc(100vh - 48px);
-  padding-top: .5rem;
-  overflow-x: hidden;
-  overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-}
-
-.sidebar .nav-link {
-  font-weight: 500;
-  color: #999;
-  stroke: #cccccc;
-}
-
-.sidebar .nav-link .feather {
-  margin-right: 4px;
-}
-
-.sidebar .nav-link.active {
-  color: #007bff;
-  stroke: #007bff;
-}
-
-.sidebar .nav-link:hover .feather,
-.sidebar .nav-link.active .feather {
-  color: inherit;
-  stroke: inherit;
-}
-
-.sidebar-heading {
-  font-size: .75rem;
-  text-transform: uppercase;
-}
-
-/*
- * Navbar
- */
-
-.navbar-brand {
-  padding-top: .75rem;
-  padding-bottom: .75rem;
-  font-size: 3rem;
-  font-weight: 900;
-  text-align: center;
-  background-color: rgba(0, 0, 0, .25);
-  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
-}
-
-.navbar-text {
-  padding-top: .75rem;
-  padding-bottom: .75rem;
-  font-size: 2rem;
-}
-
-.navbar .form-control {
-  padding: .75rem 1rem;
-  border-width: 0;
-  border-radius: 0;
 }
 
 </style>
---
+++ src/components/EYNavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="EYNavBar">
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap m-0 p-0">
+      <h2 class="navbar-brand col-sm-3 col-md-2">{{ brand }}</h2>
+      <span class="navbar-text"><small>{{ subtitle }}: </small>{{ title }}</span>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <button type="button" class="btn btn-outline-info">
+            Salir »
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EYNavBar",
+  props: {
+    title: String,
+  }, 
+  data() {
+    return {
+      brand: "EY",
+      subtitle: "In<strong>TAX</strong>lligence"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+
+.navbar-brand {
+  padding-top: .75rem;
+  padding-bottom: .75rem;
+  font-size: 3rem;
+  font-weight: 900;
+  text-align: center;
+  background-color: rgba(0, 0, 0, .25);
+  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
+}
+
+.navbar-text {
+  padding-top: .75rem;
+  padding-bottom: .75rem;
+  font-size: 2rem;
+}
+
+.navbar .form-control {
+  padding: .75rem 1rem;
+  border-width: 0;
+  border-radius: 0;
+}
+
+</style>
