e1be5528acd0eddc6a35e859e17b5f1d9534a8fd
Juan Valera <juan.valera@gmail.com>
2019-01-08 15:08:49 -0600

    FIX: Use mounted event hook

--- src/components/ChartPage.vue
+++ src/components/ChartPage.vue
@@ -18,42 +18,39 @@ export default {
     title: String,
   },
   data() {
-    this.drawChart();
     return {
       msg: "Hello beauty Vue.js!"
       
     };
   },
-  methods: {
-    drawChart: function () {
-      var ctx = document.getElementById("ChartPage");
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-          datasets: [{
-            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-            lineTension: 0,
-            backgroundColor: 'transparent',
-            borderColor: '#007bff',
-            borderWidth: 4,
-            pointBackgroundColor: '#007bff'
+  mounted: function () {
+    var ctx = document.getElementById("ChartPage");
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        datasets: [{
+          data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
           }]
         },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: false
-              }
-            }]
-          },
-          legend: {
-            display: false,
-          }
+        legend: {
+          display: false,
         }
-      });
-    },
+      }
+    });
   },
 };
 </script>
