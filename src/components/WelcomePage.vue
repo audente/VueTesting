cc114e7fa6ec29c1fb2c04635b9a89377fa9e4c5
Juan Valera <juan.valera@gmail.com>
2019-01-08 17:17:20 -0600

    ADD: Base chart. Update: Tiles

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,12 +1,10 @@
 <template>
   <EYLayout :title="title">
     <div id="WelcomePage">
-      <div class="row">
-        <TileComponent metric="$ 12.1" concept="Ingresos" detail="Ingresos Nominales MDP" icon="circle"></TileComponent>
-        <TileComponent metric="$ 12.2" concept="Ingresos" detail="Ingresos Nominales MDP" icon="activity"></TileComponent>
+      <div class="row"> <TileComponent :metric="metrics.summary1.amount" :concept="metrics.summary1.name" :detail="metrics.summary1.description" icon="bar-chart-2"></TileComponent> <TileComponent :metric="metrics.summary2.amount" :concept="metrics.summary2.name" :detail="metrics.summary2.description" icon="bar-chart-2"></TileComponent> <TileComponent :metric="metrics.summary3.amount" :concept="metrics.summary3.name" :detail="metrics.summary3.description" icon="bar-chart-2"></TileComponent>
       </div>
       <div class="row">
-        <TileComponent metric="$ 12.3" concept="Ingresos" detail="Ingresos Nominales MDP" icon="airplay"></TileComponent>
+        <canvas id="main-chart">{{ msg }}</canvas>
       </div>
     </div>
   </EYLayout>
@@ -15,6 +13,8 @@
 <script>
 import EYLayout from "./EYLayout";
 import TileComponent from "./TileComponent";
+import Chart from "chart.js";
+
 
 export default {
   name: "WelcomePage",
@@ -27,14 +27,48 @@ export default {
   },
   data() {
     return {
-      msg1: "Llenado de Declaración",
+      metrics: {
+        summary1: {name: "Ingresos", description: "Ingresos Nominales MDP", amount:"$ 13.235"},
+        summary2: {name: "ISR", description: "ISR Retenido MDP", amount:"$ 0.317"},
+        summary3: {name: "Coef", description: "Coeficiente de Utilidad", amount:"0.04"},
+      }
     };
   },
   methods: {
     openChart: function (event) {
         this.$router.push({name:'chart'});
     }
-  }
+  },
+  mounted: function () {
+  var ctx = document.getElementById("main-chart");
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      datasets: [{
+        data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false,
+      }
+    }
+  });
+},
+
 };
 </script>
 
